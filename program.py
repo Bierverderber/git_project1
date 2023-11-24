27e4689d0934067614aaac48df87067b76c36963
Арсен <arsenmkrt2006@mail.ru>
2023-11-24 21:52:14 +0300

    Another day - another commit

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 def main():     # New day — new print
     print('My first git program')
     print('And I change it every day')
+    print('Another day - another commit')
 
 
 if __name__ == '__main__':
