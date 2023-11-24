67fcffc57f463fc6b45a17adb83c97efbfe26947
Bierverderber <143602554+Bierverderber@users.noreply.github.com>
2023-11-24 21:56:17 +0300

    First commit on GitHub

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():     # New day — new print
     print('My first git program')
     print('And I change it every day')
     print('Another day - another commit')
+    print('I made this text on GitHub!')
 
 
 if __name__ == '__main__':
